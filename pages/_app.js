6d4d1a6cc0fd21844dd6c2a1dcc43ae3d4ed6d44
cmamon <christophe.quenette@gmail.com>
2021-01-08 02:13:53 +0100

    Remove useless code for meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,6 @@
-import { useEffect } from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  // useEffect(() => {
-  //   // Register a service worker to enable PWA
-  //   if ('serviceWorker' in navigator) {
-  //     window.addEventListener('load', () => {
-  //       navigator.serviceWorker.register('/service-worker.js')
-  //       .then((registration) => {
-  //         console.log('Service Worker registration successful with scope: ', registration.scope);
-  //       })
-  //       .catch((err) => {
-  //         console.log('Service Worker registration failed: ', err);
-  //       });
-  //     });
-  //   }
-  // });
-
   return <Component {...pageProps} />
 }
 
--- pages/hit-count.js
+++ pages/hit-count.js
@@ -9,12 +9,7 @@ function HitCount() {
     <>
       <Head>
         <title>Hit Counter - CV {name}</title>
-        <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content={`Hit Counter - CV ${name}`} key="title"/>
-        <meta property="og:description" content="Curriculum Vitae de Christophe QUENETTE" key="description" />
-        <meta property="og:author" content="Christophe QUENETTE" key="author" />
-        <meta property="og:image" content="https://cv.cmamon.vercel.app/images/profile-image.jpg" />
-        <meta property="og:url" content="https://cv.cmamon.vercel.app/hit-counter" key="url" />
         <meta property="og:viewport" content="width=device-width, initial-scale=1.0" key="viewport" />
       </Head>
       <Counter />
